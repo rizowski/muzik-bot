de133d61622607e171a705ea1e1604422629e8a8
Colten Rouska <colten.rouska@vivintsolar.com>
2017-04-24 00:21:18 -0600

    Changing formatting and adding more tests

--- tests/emitter.js
+++ tests/emitter.js
@@ -2,6 +2,9 @@ import emitter from '../lib/emitter';
 import { expect } from 'chai';
 
 describe('emitter', () => {
+  beforeEach(() =>{
+    emitter.removeAllListeners('test');
+  });
 
   it('does promises with on', () =>{
     const promise = emitter.on('test', (string) =>{
@@ -23,5 +26,25 @@ describe('emitter', () => {
 
     emitter.emit('test', 'testing');
     return promise;
+  });
+
+  it("does not throw any errors if a promise isn't returned", (done) => {
+    emitter.once('test', (data) =>{
+      expect(data).to.equal('yes');
+      done();
+    });
+
+    emitter.emit('test', 'yes');
+  });
+
+  it('rejects a promise if an exception is thrown', (done) => {
+    emitter.once('test', () =>{
+      throw new Error('twerks');
+    }).catch(e =>{
+      expect(e.message).to.equal('twerks');
+      done();
+    });
+
+    emitter.emit('test', 'yes');
   });
 });

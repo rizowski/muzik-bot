fe497b8485641e68a12220be6c45156f4a3dcbbb
Zack Smith <zack.smith@towerswatson.com>
2017-08-11 15:14:12 -0600

    WIP: Begin moving event emitter to observable implementation. This commit changes the hello command to use Observables, changes the discord client to listen to events as observables, and changes the bot.js to send events from discord client through observable pipeline out to commands.

--- tests/lib/commands/hello-test.js
+++ tests/lib/commands/hello-test.js
@@ -1,58 +1,18 @@
 import { expect } from 'chai';
-import '../../../lib/commands/';
-import emitter from '../../../lib/emitter';
-import { bot } from '../../../lib/events';
+import helloCommand from '../../../lib/commands/bot/hello';
 
-describe('command: hello', () => {
-  let discord;
+describe.only('command: hello', () => {
   const responses = ['Hi', 'Hello', ':wave:', 'Hi there.', 'Whachuwant?', 'Howdy', 'Greetings', 'Yo dawg', ':kappa:'];
 
-  before(() => {
-    emitter.removeAllListeners(bot.response);
+  it('returns some response', (done) => {
+    helloCommand.action({ payload: {} })
+      .subscribe(
+        ({ response }) => {
+          expect(responses).to.include(response);
+        },
+        done, // error
+        done // complete
+      );
   });
 
-  beforeEach(() => {
-    discord = {
-      voiceChannel: 'blue'
-    };
-  });
-
-  it(`calls ${bot.response} when command:hello is emitted`, () => {
-    const input = { string: '' };
-    const handler = ({ response }) => {
-      expect(responses).to.include(response);
-    };
-
-    const promise = emitter.once(bot.response, handler);
-
-    emitter.emit('command:hello', { input, discord });
-
-    return promise;
-  });
-
-  it(`calls ${bot.response} when command:hi is emitted`, () => {
-    const input = { string: '' };
-    const handler = ({ response }) => {
-      expect(responses).to.include(response);
-    };
-
-    const promise = emitter.once(bot.response, handler);
-
-    emitter.emit('command:hi', { input, discord });
-
-    return promise;
-  });
-
-  it(`calls ${bot.response} when command:yo is emitted`, () => {
-    const input = { string: '' };
-    const handler = ({ response }) => {
-      expect(responses).to.include(response);
-    };
-
-    const promise = emitter.once(bot.response, handler);
-
-    emitter.emit('command:yo', { input, discord });
-
-    return promise;
-  });
 });

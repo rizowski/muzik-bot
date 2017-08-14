79262ba0475f3e8175236bbc69aae301ce9401d3
Zack Smith <zack.smith@towerswatson.com>
2017-08-14 13:10:08 -0600

    Fixed up help command to use Observables instead of event emitter. Updated tests to reflect changed.

--- tests/lib/commands/hello-test.js
+++ tests/lib/commands/hello-test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import helloCommand from '../../../lib/commands/bot/hello';
 
-describe.only('command: hello', () => {
+describe('command: hello', () => {
   const responses = ['Hi', 'Hello', ':wave:', 'Hi there.', 'Whachuwant?', 'Howdy', 'Greetings', 'Yo dawg', ':kappa:'];
 
   it('returns some response', (done) => {
--- tests/lib/commands/help-test.js
+++ tests/lib/commands/help-test.js
@@ -1,144 +1,145 @@
 import { expect } from 'chai';
-import '../../../lib/commands/';
-import emitter from '../../../lib/emitter';
-import { bot, commands } from '../../../lib/events';
+import helpCommand from '../../../lib/commands/bot/help';
 
 describe('command: help', () => {
-  let discord;
-
-  before(() => {
-    emitter.removeAllListeners(bot.response);
-  });
-
-  beforeEach(() => {
-    discord = {
-      voiceChannel: 'blue',
+  const discord = {
+    voiceChannel: 'blue',
+    original: {
       client: {
         user: {
-          username: 'testy'
-        }
-      }
-    };
-  });
+          username: 'testy',
+        },
+      },
+    },
+  };
 
   describe('base help', () => {
-    it(`calls ${bot.response} when ${commands.help} is emitted`, () => {
+    it('shows help text for help command', (done) => {
       const input = { string: '' };
       const handler = ({ response }) => {
-        expect(response).to.equal('I have the following commands:\n```play - Plays some tunes\nqueued - Shows what is queued\nshuffle - Shuffles all the songs in the current queue\nskip - Skips something nasty\nstop - Clears the Queue\nhello - Says hello\nhelp - Gives you some less than helpful information\nset - Used for changing settings```\nJust `@testy` with your command and I will oblige.\nYou can also `@testy help <command>` to get more information about a particular command');
+        expect(response).to.equal(
+          'I have the following commands:\n```play - Plays some tunes\nqueued - Shows what is queued\nshuffle - Shuffles all the songs in the current queue\nskip - Skips something nasty\nstop - Clears the Queue\nhello - Says hello\nhelp - Gives you some less than helpful information\nclear - Clears the current cached songs\nset - Used for changing settings```\nJust `@testy` with your command and I will oblige.\nYou can also `@testy help <command>` to get more information about a particular command'
+        );
       };
 
-      const promise = emitter.once(bot.response, handler);
+      helpCommand
+        .action({ input, payload: discord })
+        .subscribe(handler, done, done);
+    });
 
-      emitter.emit(commands.help, { input, discord });
+    it('shows default help text for unknown sub-command', (done) => {
+      const input = { string: 'command-that-will-never-be-implemented' };
+      const handler = ({ response }) => {
+        expect(response).to.equal(
+          'I have the following commands:\n```play - Plays some tunes\nqueued - Shows what is queued\nshuffle - Shuffles all the songs in the current queue\nskip - Skips something nasty\nstop - Clears the Queue\nhello - Says hello\nhelp - Gives you some less than helpful information\nclear - Clears the current cached songs\nset - Used for changing settings```\nJust `@testy` with your command and I will oblige.\nYou can also `@testy help <command>` to get more information about a particular command'
+        );
+      };
 
-      return promise;
+      helpCommand
+        .action({ input, payload: discord })
+        .subscribe(handler, done, done);
     });
   });
 
   describe('help', () => {
     describe('<help>', () => {
-      it(`calls ${bot.response} when ${commands.help} help is emitted`, () => {
+      it('shows help text for help sub-command', done => {
         const input = { string: 'help' };
         const handler = ({ response }) => {
-          expect(response).to.equal('`help` - Gives you some less than helpful information\n```help [command]```');
+          expect(response).to.equal(
+            '`help` - Gives you some less than helpful information\n```help [command]```'
+          );
         };
 
-        const promise = emitter.once(bot.response, handler);
-
-        emitter.emit(commands.help, { input, discord });
-
-        return promise;
+        helpCommand
+          .action({ input, payload: discord })
+          .subscribe(handler, done, done);
       });
     });
 
-    describe('<hello>', () =>{
-      it(`calls ${bot.response} when ${commands.help} hello is emitted`, () => {
+    describe('<hello>', () => {
+      it('shows help text for hello sub-command', done => {
         const input = { string: 'hello' };
         const handler = ({ response }) => {
-          expect(response).to.equal('`hello` - Says hello\n``````hello\nhi\nyo``````');
+          expect(response).to.equal('`hello` - Says hello\n``````hello``````');
         };
 
-        const promise = emitter.once(bot.response, handler);
-
-        emitter.emit(commands.help, { input, discord });
-
-        return promise;
+        helpCommand
+          .action({ input, payload: discord })
+          .subscribe(handler, done, done);
       });
     });
 
-    describe('<play>', () =>{
-      it(`calls ${bot.response} when ${commands.help} play is emitted`, () => {
+    describe('<play>', () => {
+      it('shows help text for play sub-command', done => {
         const input = { string: 'play' };
         const handler = ({ response }) => {
-          expect(response).to.equal('`play` - Plays some tunes\n```play <url>\nplay <playlistUrl>\nplay <search terms>```');
+          expect(response).to.equal(
+            '`play` - Plays some tunes\n```play <url>\nplay <playlistUrl>\nplay <search terms>```'
+          );
         };
 
-        const promise = emitter.once(bot.response, handler);
-
-        emitter.emit(commands.help, { input, discord });
-
-        return promise;
+        helpCommand
+          .action({ input, payload: discord })
+          .subscribe(handler, done, done);
       });
     });
 
-    describe('<queued>', () =>{
-      it(`calls ${bot.response} when ${commands.help} queued is emitted`, () => {
+    describe('<queued>', () => {
+      it('shows help text for queued sub-command', done => {
         const input = { string: 'queued' };
         const handler = ({ response }) => {
-          expect(response).to.equal('`queued` - Shows what is queued\n````queued````');
+          expect(response).to.equal(
+            '`queued` - Shows what is queued\n````queued````'
+          );
         };
 
-        const promise = emitter.once(bot.response, handler);
-
-        emitter.emit(commands.help, { input, discord });
-
-        return promise;
+        helpCommand
+          .action({ input, payload: discord })
+          .subscribe(handler, done, done);
       });
     });
 
-    describe('<shuffle>', () =>{
-      it(`calls ${bot.response} when ${commands.help} shuffle is emitted`, () => {
+    describe('<shuffle>', () => {
+      it('shows help text for shuffle sub-command', done => {
         const input = { string: 'shuffle' };
         const handler = ({ response }) => {
-          expect(response).to.equal('`shuffle` - Shuffles all the songs in the current queue\n````shuffle````');
+          expect(response).to.equal(
+            '`shuffle` - Shuffles all the songs in the current queue\n````shuffle````'
+          );
         };
 
-        const promise = emitter.once(bot.response, handler);
-
-        emitter.emit(commands.help, { input, discord });
-
-        return promise;
+        helpCommand
+          .action({ input, payload: discord })
+          .subscribe(handler, done, done);
       });
     });
 
-    describe('<skip>', () =>{
-      it(`calls ${bot.response} when ${commands.help} skip is emitted`, () => {
+    describe('<skip>', () => {
+      it('shows help text for skip sub-command', done => {
         const input = { string: 'skip' };
         const handler = ({ response }) => {
-          expect(response).to.equal('`skip` - Skips something nasty\n````skip````');
+          expect(response).to.equal(
+            '`skip` - Skips something nasty\n````skip````'
+          );
         };
 
-        const promise = emitter.once(bot.response, handler);
-
-        emitter.emit(commands.help, { input, discord });
-
-        return promise;
+        helpCommand
+          .action({ input, payload: discord })
+          .subscribe(handler, done, done);
       });
     });
 
-    describe('<stop>', () =>{
-      it(`calls ${bot.response} when ${commands.help} stop is emitted`, () => {
+    describe('<stop>', () => {
+      it('shows help text for stop sub-command', done => {
         const input = { string: 'stop' };
         const handler = ({ response }) => {
           expect(response).to.equal('`stop` - Clears the Queue\n````stop````');
         };
 
-        const promise = emitter.once(bot.response, handler);
-
-        emitter.emit(commands.help, { input, discord });
-
-        return promise;
+        helpCommand
+          .action({ input, payload: discord })
+          .subscribe(handler, done, done);
       });
     });
   });
